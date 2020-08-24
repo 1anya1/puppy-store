54846026278d7fe7ee133f053674a57cd51b4322
Anna <filatovaa@yahoo.com>
2020-08-23 22:52:14 -0700

    search functionallity added and data search added for alphabetical search and numerical search

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import data from './data'
 class PuppyStore extends React.Component {
   state={
     data:[],
+    filter:''
   }
 
   componentDidMount(){
@@ -22,14 +23,32 @@ class PuppyStore extends React.Component {
     })
     this.setState({data:like})
   }
+  alphabeticalSort=()=>{
+    this.setState(
+      this.state.data.sort((a,b)=>a.name.localeCompare(b.name)))
+    }
+  likeSort=()=>{
+    this.setState(
+      this.state.data.sort((a,b)=>b.likes-a.likes)
+    )
+  }
+  handleChange=(event)=>{
+    console.log(this.handleChange)
+    this.setState({filter: event.target.value})
+  }
   render(){
-  
-    console.log(this.state.data)
+    const filter = this.state.filter;
+    
     return (
       <div>
         <h1>Puppy Store</h1>
+        <input  className='search' placeholder='SEARCH' value={filter} onChange={this.handleChange}></input>
+            <button onClick={this.alphabeticalSort}value ='likes' selected>Alphabetical</button>
+            <button onClick={this.likeSort}value ='likes' selected>Popularity</button>
+           
+            
         <div className='container'>
-        <DisplayItem  data={this.state.data} handleLike={this.handleLike}/>
+        <DisplayItem  data={this.state.data} handleLike={this.handleLike} filter={this.state.filter}/>
 
         </div>
         
@@ -43,17 +62,27 @@ class PuppyStore extends React.Component {
 class DisplayItem extends React.Component{
 
   render(){
-    let item = this.props.data
+    let items = this.props.data
+    const filter = this.props.filter;
+    console.log(filter)
+    const lowercasedFilter= filter.toLowerCase();
+    const filteredData=items.filter(item=>{
+      return Object.keys(item).some(key=>
+        typeof item[key]=== 'string' && item[key].toLowerCase().includes(lowercasedFilter)
+        )
+    })
   
     
     return(
-      item.map(el=>{
+      filteredData.map(el=>{
         return(
           <div className='card' key={el.id}>
             <div className='name'>{el.name}</div>
-            <div className='description'>{el.description}</div>
-           
-              <img className='image' src={el.img}></img>
+            <div className='description'><b>Description:</b> <br/><br/>{el.description}</div>
+            <div className='image'>
+                <img className='images' src={el.img}></img>
+            </div>
+              
               <div className='price'>{el.price}</div>
               <div>
               <button className='likeButton' onClick={()=>this.props.handleLike(el.id)}>Like</button>
--- src/data.js
+++ src/data.js
@@ -1,7 +1,11 @@
 
 const data=[
     {id: 1, name:'Teething Rings', price:'$1.99', description:'Good for teeth ', img:'./photos/chewToy.png', likes:99},
-    { id: 2, name:'chew toy', price:'$1.99', description:'Good for teeth', img:'./photos/chewToy.png', likes:14}
+    { id: 2, name:'Harnest', price:'$10.99', description:'Easy to put on, great for training', img:'./photos/harnest.png', likes:14},
+    { id: 3, name:'Leash', price:'$15.99', description:'Retractable leash', img:'./photos/leash.png', likes:70},
+    { id: 4, name:'Pee-Pads', price:'$30.99', description:'ETraining pads for puppies', img:'./photos/peepads.png', likes:140},
+    { id: 5, name:'Plush Toy', price:'$17.99', description:'Hide and seek toy', img:'./photos/toy.png', likes:14},
+    { id: 6, name:'Chewy Toy', price:'$7.99', description:'Chewy toy for teething puppies', img:'./photos/chewToy2.png', likes:50}
 ]
 
 
