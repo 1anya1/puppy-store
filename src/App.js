312e65783565dc3f8e0f92e9502f58ef5df7da93
Anna <filatovaa@yahoo.com>
2020-08-24 19:52:35 -0700

    updates to layout and styling

--- src/App.js
+++ src/App.js
@@ -32,31 +32,28 @@ class PuppyStore extends React.Component {
       this.state.data.sort((a,b)=>b.likes-a.likes)
     )
   }
-  handleChange=(event)=>{
+  handleChangeSort=(event)=>{
     console.log(this.handleChange)
     this.setState({filter: event.target.value})
   }
+
   render(){
     const filter = this.state.filter;
     
     return (
       <div>
-        <h1>Puppy Store</h1>
-        <input  className='search' placeholder='SEARCH' value={filter} onChange={this.handleChange}></input>
-            <button onClick={this.alphabeticalSort}value ='likes' selected>Alphabetical</button>
-            <button onClick={this.likeSort}value ='likes' selected>Popularity</button>
-           
-            
+        <h1 className='header'>Puppy Store</h1>
+        <div className='searchComponents'>
+            <input className='search' placeholder='SEARCH' value={filter} onChange={this.handleChangeSort}></input>
+            <button className='searchButton' onClick={this.alphabeticalSort} value ='alpha' >A_Z</button>
+            <button className='searchButton' onClick={this.likeSort}value ='likes' >Popularity</button>
+        </div>      
         <div className='container'>
-        <DisplayItem  data={this.state.data} handleLike={this.handleLike} filter={this.state.filter}/>
-
-        </div>
-        
+          <DisplayItem  data={this.state.data} handleLike={this.handleLike} filter={this.state.filter}/>
+        </div> 
       </div>
     )
-
-  }
-  
+  }  
 }
 
 class DisplayItem extends React.Component{
@@ -64,44 +61,40 @@ class DisplayItem extends React.Component{
   render(){
     let items = this.props.data
     const filter = this.props.filter;
-    console.log(filter)
     const lowercasedFilter= filter.toLowerCase();
     const filteredData=items.filter(item=>{
       return Object.keys(item).some(key=>
         typeof item[key]=== 'string' && item[key].toLowerCase().includes(lowercasedFilter)
         )
     })
-  
-    
+
     return(
       filteredData.map(el=>{
         return(
           <div className='card' key={el.id}>
-            <div className='name'>{el.name}</div>
-            <div className='description'><b>Description:</b> <br/><br/>{el.description}</div>
+            <div className='name'>
+              <h2>{el.name}</h2>
+              <button className='likeButton' onClick={()=>this.props.handleLike(el.id)}>
+                <p className='totalLikes'>{el.likes}</p>
+              </button>
+            </div>
+            <div className='description'>
+              <h3 className='description-header'>Description:</h3>
+                <br/><br/>
+                  {el.description}
+                  <br/><br/>
+                  <b>{el.price}</b>
+            </div>
             <div className='image'>
                 <img className='images' src={el.img}></img>
             </div>
-              
-              <div className='price'>{el.price}</div>
-              <div>
-              <button className='likeButton' onClick={()=>this.props.handleLike(el.id)}>Like</button>
-              <div>{el.likes}</div>
-
-
-              </div>
-              
-            
-            
+              <div className='add-to-cart'>
+              <button className='cart-button'>Add To Cart</button>
+            </div>    
           </div>
         )
-  
       })
-     
-
-    )
-  
-    
+    )  
   }
 }
 
--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 
 const data=[
-    {id: 1, name:'Teething Rings', price:'$1.99', description:'Good for teeth ', img:'./photos/chewToy.png', likes:99},
-    { id: 2, name:'Harnest', price:'$10.99', description:'Easy to put on, great for training', img:'./photos/harnest.png', likes:14},
-    { id: 3, name:'Leash', price:'$15.99', description:'Retractable leash', img:'./photos/leash.png', likes:70},
-    { id: 4, name:'Pee-Pads', price:'$30.99', description:'ETraining pads for puppies', img:'./photos/peepads.png', likes:140},
-    { id: 5, name:'Plush Toy', price:'$17.99', description:'Hide and seek toy', img:'./photos/toy.png', likes:14},
-    { id: 6, name:'Chewy Toy', price:'$7.99', description:'Chewy toy for teething puppies', img:'./photos/chewToy2.png', likes:50}
+    {id: 1, name:'Teething Rings', price:'$1.99', description:'These chews can help reduce destructive chewing behavior while providing healthy benefits. Great taste of chicken in each and every bite.', img:'./photos/chewToy.png', likes:99},
+    { id: 2, name:'Harnest', price:'$10.99', description:'Soft, stretchy, and resilient. A cushioned, lightweight harness that’s simple to secure and easy to link up with.', img:'./photos/harnest.png', likes:14},
+    { id: 3, name:'Leash', price:'$15.99', description:'The Flexi New Comfort Medium Retractable Dog Leash measures 16 feet, and comes with an adjustable handle for comfort and convenience.', img:'./photos/leash.png', likes:70},
+    { id: 4, name:'Pee-Pads', price:'$30.99', description:'Super absorbent pads are specially treated to attract your puppy like magic. Pup will be naturally attracted to the pad.', img:'./photos/peepads.png', likes:140},
+    { id: 5, name:'Plush Toy', price:'$17.99', description:'This toy comes in a variety of adorable food-related options including milk and cookies, a popcorn bucket, coffee and donuts. ', img:'./photos/toy.png', likes:14},
+    { id: 6, name:'Chewy Toy', price:'$7.99', description:'Bristles raised during chewing will help clean teeth and prevent tartar build up. Designed for teething puppies.', img:'./photos/chewToy2.png', likes:50}
 ]
 
 
