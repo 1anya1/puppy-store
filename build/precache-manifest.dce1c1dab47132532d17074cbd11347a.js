988c279c247137aecac709ff84e2e3dd2a8f1a1c
Anna <filatovaa@yahoo.com>
2020-08-24 19:56:07 -0700

    react file build"

---
+++ build/precache-manifest.dce1c1dab47132532d17074cbd11347a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c103ae0e68446d307100a4985a63d5ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a220bfb1ac41e85b178c",
+    "url": "/static/css/main.6ebb3e5c.chunk.css"
+  },
+  {
+    "revision": "f36f0f3411ea0ea46641",
+    "url": "/static/js/2.b6dfd74c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6dfd74c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a220bfb1ac41e85b178c",
+    "url": "/static/js/main.44d11885.chunk.js"
+  },
+  {
+    "revision": "4f32f96e369131633a98",
+    "url": "/static/js/runtime-main.9bbc6588.js"
+  },
+  {
+    "revision": "9f61111962cc66e53f0c907d4fcc17cb",
+    "url": "/static/media/heart.9f611119.png"
+  }
+]);
